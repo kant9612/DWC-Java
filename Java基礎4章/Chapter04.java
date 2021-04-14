089fb687e7200cc949f98314c7e45e7523110e8f
kant9612 <kanstephentakayama@gmail.com>
2021-04-14 03:04:00 +0000

    Java基礎4章確認問題

---
+++ Java基礎4章/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "kan";
+		System.out.println(name);
+
+		name = "み";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
