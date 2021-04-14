efcb560dcb0fd66f2c9d7395c0777f0138cfa3d9
kant9612 <kanstephentakayama@gmail.com>
2021-04-14 08:03:44 +0000

    Java基礎5章確認問題

---
+++ Java基礎5章/Chapter05.java
@@ -0,0 +1,30 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 9223372036854775807L;
+		double double1 = 1234567.89;
+		boolean bool1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(bool1);
+
+		Float float1 = 0.123f;
+		Integer integer1 = 5;
+		Character char1 = 'あ';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(char1);
+
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+
+//		int[] array1 = new int[5];  // 配列の宣言と生成
+//		array1[0] = 60;  // インデックス番号「0」を指定して１番目の要素にデータを代入
+//		array1[2] = 20;  // ２番目の要素より先に３番目の要素にデータを代入
+//		array1[1] = 20;  // ３番目と同じ値を２番目の要素に代入
+//		array1[3] = 40;
+//		array1[4] = 10;
+//		int int2 = array1[3];  // インデックス番号「2」を指定して３番目の要素を取得
+//		System.out.println(int2);
+	}
+}
