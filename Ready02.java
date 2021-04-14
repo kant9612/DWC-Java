da2633763ff6556ac0f273757a78e4be9ced1636
kant9612 <kanstephentakayama@gmail.com>
2021-04-14 03:00:10 +0000

    Java基礎3章 確認問題

---
+++ Ready02.java
@@ -0,0 +1,10 @@
+
+public class Ready02 {
+	public static void main(String[] args) {
+		System.out.println("Hello WEBCAMP!\n"
+				+ "Hello Java!");
+//		System.out.println("Hello WEBCAMP!");
+//		System.out.println("Hello Java!");
+//		上記2行でも可
+	}
+}
