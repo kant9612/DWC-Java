65cde7b96d72a21f2eec12563bd788b5af7a2fa9
kant9612 <kanstephentakayama@gmail.com>
2021-04-29 03:27:26 +0000

    Java基礎9章　Chapter09が読み込まれない原因が知りたい

--- Java基礎9章/practice/Car.java
+++ Java基礎9章/practice/Car.java
@@ -2,21 +2,21 @@ package practice;
 
 public class Car {
 	// 燃費（Km/L）
-    private double fuelCost;
-    // 残量（L）
-    private double fuelAmount;
-
-    public Car(double fuelCost, double fuelAmount) {
-        this.fuelCost = fuelCost;
-        this.fuelAmount = fuelAmount;
-    }
-
-    public void move(int km) {
-        System.out.println(km + " km 走ります");
-        this.fuelAmount -= (km / fuelCost);
-    }
-
-    public double getFuelAmount() {
-        return this.fuelAmount;
-    }
+	private double fuelCost;
+	// 残量
+	private double fuelAmount;
+	//コンストラクタを作成
+	public Car(double fuelCost, double fuelAmount){
+		this.fuelCost = fuelCost;
+		this.fuelAmount = fuelAmount;
+	}
+	//moveメソッド
+	public void move(int km){
+		System.out.println(km + "km走ります。");
+		this.fuelAmount -= (km / fuelCost);
+	}
+	//return
+	public double getFuelAmount(){
+		return this.fuelAmount;
+	}
 }
